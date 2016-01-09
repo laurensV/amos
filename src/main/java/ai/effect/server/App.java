d94bb6ec94236709c86641c9e9fbd6036143a6e7
Jesse Eisses <jesse@myadbooker.com>
2016-01-09 15:08:33 +0100

    Set CORS allowed for DNA servlets

--- src/main/java/ai/effect/server/App.java
+++ src/main/java/ai/effect/server/App.java
@@ -1,11 +1,16 @@
 package ai.effect.server;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 
 import ai.effect.servlet.ClickServlet;
-import ai.effect.servlet.ExampleServlet;
 import ai.effect.servlet.InitServlet;
 import ai.effect.servlet.StartServlet;
 import ai.effect.servlet.StopServlet;
@@ -20,6 +25,12 @@ public class App {
     	ServletContextHandler context = new ServletContextHandler();
     	context.setContextPath("/dna");
     	server.setHandler(context);
+    	
+    	FilterHolder cors = context.addFilter(CrossOriginFilter.class,  "/*",  EnumSet.of(DispatcherType.REQUEST));
+    	cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+    	cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+    	cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET");
+    	cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin");
     	
     	context.addServlet(new ServletHolder(new InitServlet()), "/init");
     	context.addServlet(new ServletHolder(new ClickServlet()), "/click");
