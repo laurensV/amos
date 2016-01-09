2c5bc30083d91e7fdc9764821fbf3d3958760a91
Jesse Eisses <jesse@myadbooker.com>
2016-01-09 16:39:31 +0100

    Return dummy JSON on init servlet

--- src/main/java/ai/effect/servlet/InitServlet.java
+++ src/main/java/ai/effect/servlet/InitServlet.java
@@ -9,6 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 public class InitServlet extends DnaServlet {
 	protected String getResponse() {
-		return "Click";
+		return "{\"session-id\": 223423423423, \"items\": [{\"id\": \".btn\", \"attributes\": [{\"attribute\": \"background-color\", \"value\": \"blue\"}]}]}";
 	}
 }
